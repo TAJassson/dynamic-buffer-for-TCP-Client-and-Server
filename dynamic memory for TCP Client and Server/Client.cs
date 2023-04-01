7ff47a0585efe8032d29ee06f4d6a66749fd6eb8
Jason Tse <despa.tse@gmail.com>
2023-04-01 15:32:10 +0800

    加入專案檔案。

---
+++ dynamic memory for TCP Client and Server/Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace dynamic_memory_for_TCP_Client_and_Server
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("TCP dynamic buffer size for server and client side");
+        }
+        public static void clientside()
+        {
+            string Host = Dns.GetHostName();
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send("IPAddress"); //Type IP Address here
+            string rdip = reply.Address.ToString();
+            string IPAddress = rdip;
+            int Port = 1234;
+            string Filename = "Your filename or file path"; 
+            byte[] buffer = null;
+            byte[] header = null;
+            FileStream fs = new FileStream(Filename, FileMode.Open);
+            bool read = true;
+            long fileSize = fs.Length;
+            int bufferSize = 8192; // 設定初始緩衝區大小
+            if (fileSize > 1024 * 1024 * 5) // 如果檔案大於 10 MB，則增加緩衝區大小
+            {
+                int fileSizeMB = (int)(fileSize / (1024 * 1024));
+                bufferSize = fileSizeMB * 2048;
+            }
+            int bufferCount = Convert.ToInt32(Math.Ceiling((double)fs.Length / (double)bufferSize));
+            TcpClient tcpClient = new TcpClient(IPAddress, Port);
+            tcpClient.SendTimeout = 600000;
+            tcpClient.ReceiveTimeout = 600000;
+            string headerStr = "Content-length:" + fs.Length.ToString() + "\r\nFilename:" + $"{Filename}\r\n";
+            header = new byte[bufferSize];
+            Array.Copy(Encoding.Unicode.GetBytes(headerStr), header, Encoding.Unicode.GetBytes(headerStr).Length);
+
+            tcpClient.Client.Send(header);
+
+            for (int i = 0; i < bufferCount; i++)
+            {
+                buffer = new byte[bufferSize];
+                int size = fs.Read(buffer, 0, bufferSize);
+
+                tcpClient.Client.Send(buffer, size, SocketFlags.Partial);
+
+            }
+            Console.WriteLine("Send hwid sccessfully, now delete socket");
+            tcpClient.Client.Close();
+
+            fs.Close();
+            File.Delete(Host);
+        }
+    }
+}
---
+++ dynamic memory for TCP Client and Server/Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace dynamic_memory_for_TCP_Client_and_Server
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("TCP dynamic buffer size for server and client side");
+        }
+
+        public static void serverside()
+        {
+
+            int Port = 1234;
+            TcpListener listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start();
+            while (true)
+            {
+                Socket socket = listener.AcceptSocket();
+                string clientIPAddress = $"{DateTime.Now}: Connected Client IP is: " + IPAddress.Parse(((IPEndPoint)socket.RemoteEndPoint).Address.ToString());
+                Console.WriteLine(clientIPAddress);
+                byte[] header = new byte[2048];
+                socket.Receive(header);
+                string headerStr = Encoding.Unicode.GetString(header);
+                string[] splitted = headerStr.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                Dictionary<string, string> headers = new Dictionary<string, string>();
+                foreach (string s in splitted)
+                {
+                    if (s.Contains(":"))
+                    {
+                        headers.Add(s.Substring(0, s.IndexOf(":")), s.Substring(s.IndexOf(":") + 1));
+                    }
+                }
+
+                if (headers.ContainsKey("Content-length") && headers.ContainsKey("Filename"))
+                {
+                    int filesize = Convert.ToInt32(headers["Content-length"]);
+                    string filename = headers["Filename"];
+                    try
+                    {
+                        FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+                        int bufferSize = 2048;
+                        int bufferCount = Convert.ToInt32(Math.Ceiling((double)filesize / (double)bufferSize));
+                        byte[] buffer = new byte[bufferSize];
+                        int bytesRead = 0;
+                        int totalBytesRead = 0;
+                        while (totalBytesRead < filesize)
+                        {
+                            bytesRead = socket.Receive(buffer, Math.Min(bufferSize, filesize - totalBytesRead), SocketFlags.None);
+                            if (bytesRead == 0)
+                            {
+                                break;
+                            }
+                            fs.Write(buffer, 0, bytesRead);
+                            totalBytesRead += bytesRead;
+                        }
+                        Console.WriteLine($"{DateTime.Now} Total received: {totalBytesRead},successfully received from Client {((IPEndPoint)socket.RemoteEndPoint).Address}");
+                        fs.Close();
+                        socket.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                    finally
+                    {
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid header format");
+                    socket.Close();
+                }
+            }
+        }
+    }
+}
